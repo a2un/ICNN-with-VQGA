e025438ad8e08b8b2122e6a320cbe16378113502
Arun Balajiee Lekshmi Narayanan <ARL122@pitt.edu>
2021-04-16 15:46:22 -0400

    categories

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -95,12 +95,16 @@ def collate_fn(data):
     # Merge images (from tuple of 3D tensor to 4D tensor).
     images = torch.stack(images, 0)
 
+    # Merge categories (from a tuple of 1D tensor to 2D tensor)
+    categories = torch.stack(categories, 0)
+    
     # Merge captions (from tuple of 1D tensor to 2D tensor).
     lengths = [len(cap) for cap in captions]
     targets = torch.zeros(len(captions), max(lengths)).long()
     for i, cap in enumerate(captions):
         end = lengths[i]
-        targets[i, :end] = cap[:end]        
+        targets[i, :end] = cap[:end]
+               
     return images, categories, targets, lengths
 
 def get_loader(img_dir, data_file, data_set, vocab, transform, batch_size, shuffle, num_workers):
