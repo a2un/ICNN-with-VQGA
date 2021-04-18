ff579fdb59692e55c8b71add231e7b670073f4f3
Arun Balajiee Lekshmi Narayanan <ARL122@pitt.edu>
2021-04-18 00:29:18 -0400

    attentionscls

--- model3.py
+++ model3.py
@@ -127,7 +127,7 @@ class DecoderRNN(nn.Module):
             # packed = pack_padded_sequence(,
             #     lengths, batch_first=True) 
             print(torch.cat([embeddings[:batch_size_t,t,:], attention_weighted_encoding]).size())
-            h, c = self.lstm(torch.cat([embeddings[:batch_size_t,t,:], attention_weighted_encoding], dim=1).unsqueeze(0),
+            h, c = self.lstm(torch.cat([embeddings[:batch_size_t,t,:], attention_weighted_encoding]).unsqueeze(0),
                             (h,c))
             outputs = self.linear(h)
         
