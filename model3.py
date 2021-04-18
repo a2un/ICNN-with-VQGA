db49d5533a5175be19a6960db848b8104a7da3d2
Arun Balajiee Lekshmi Narayanan <ARL122@pitt.edu>
2021-04-18 00:57:58 -0400

    attentions

--- model3.py
+++ model3.py
@@ -127,6 +127,7 @@ class DecoderRNN(nn.Module):
             attention_weighted_encoding = gate * attention_weighted_encoding
             # packed = pack_padded_sequence(,
             #     lengths, batch_first=True) 
+            print(torch.cat([embeddings[:batch_size_t,t,:], attention_weighted_encoding]).size())
             h, _ = self.lstm(torch.cat([embeddings[:batch_size_t,t,:], attention_weighted_encoding]),
                             h[:batch_size_t])
             outputs = self.linear(h.squeeze(0))
