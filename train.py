ab2d477af67424eb136311df3a2db30184bb1adc
Arun Balajiee Lekshmi Narayanan <ARL122@pitt.edu>
2021-04-18 19:53:25 -0400

    attention

--- train.py
+++ train.py
@@ -53,7 +53,7 @@ def main():
 				# Forward, backward and optimize
 				features = encoder(image)
 				questions = questions.to(device)
-				lengths = [len(question) for question in questions]
+				lengths = torch.from_nump(np.array([len(question) for question in questions]))
 				lenghts = lengths.to(device)
 				outputs = decoder(features, questions, lengths)
 				loss = criterion(outputs, targets)
