491ed0c0a42bfe9ec55c0bff2713c37d477c1966
Arun Balajiee Lekshmi Narayanan <ARL122@pitt.edu>
2021-04-18 17:01:52 -0400

    attention

--- model3.py
+++ model3.py
@@ -69,8 +69,8 @@ class Attention(nn.Module):
         """
         super(Attention, self).__init__()
         self.attention_dim = attention_dim
-        self.encoder_att = nn.Linear(encoder_dim, attention_dim)  # linear layer to transform encoded image
-        self.decoder_att = nn.Linear(decoder_dim, attention_dim)  # linear layer to transform decoder's output
+        self.encoder_att = nn.Linear(attention_dim, encoder_dim)  # linear layer to transform encoded image
+        self.decoder_att = nn.Linear(attention_dim, decoder_dim)  # linear layer to transform decoder's output
         self.full_att = nn.Linear(attention_dim, 1)  # linear layer to calculate values to be softmax-ed
         self.relu = nn.ReLU()
         self.softmax = nn.Softmax(dim=1)  # softmax layer to calculate weights
