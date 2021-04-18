e2c9171ae2f09e179034ccf5a0a98443a17cff7c
Arun Balajiee Lekshmi Narayanan <ARL122@pitt.edu>
2021-04-17 23:26:21 -0400

    attentions

--- model3.py
+++ model3.py
@@ -87,7 +87,7 @@ class Attention(nn.Module):
         print(encoder_out.size(),att1.size(),decoder_hidden.size(),att2.size())
         att = self.full_att(self.relu(att1 + att2.unsqueeze(0))).squeeze(2)  # (batch_size, )
         alpha = self.softmax(att)  # (batch_size, )
-        print((att1 + att2.unsqueeze(0)).size(),att.size(),alpha.size())
+        print((att1 + att2.unsqueeze(0)).size(),att.size(),alpha.unsqueeze(1))
         attention_weighted_encoding = (encoder_out * alpha.unsqueeze(1)).sum(dim=1)  # (batch_size, embed_size)
 
         return attention_weighted_encoding
