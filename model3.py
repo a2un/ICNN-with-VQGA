25d5c6087a21191f69e9fb865edadd25797eb390
Arun Balajiee Lekshmi Narayanan <ARL122@pitt.edu>
2021-04-18 00:28:14 -0400

    attentions

--- model3.py
+++ model3.py
@@ -124,10 +124,9 @@ class DecoderRNN(nn.Module):
             attention_weighted_encoding = self.attention(embeddings[:batch_size_t,t,:],h)
             gate = self.sigmoid(self.f_beta(h))  # gating scalar, (batch_size_t, encoder_dim)
             attention_weighted_encoding = gate * attention_weighted_encoding
-            print(gate.size(), attention_weighted_encoding.size())
             # packed = pack_padded_sequence(,
             #     lengths, batch_first=True) 
-            print(h.size(),c.size())
+            print(torch.cat([embeddings[:batch_size_t,t,:], attention_weighted_encoding], dim=1).size())
             h, c = self.lstm(torch.cat([embeddings[:batch_size_t,t,:], attention_weighted_encoding], dim=1).unsqueeze(0),
                             (h,c))
             outputs = self.linear(h)
