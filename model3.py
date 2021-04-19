b7e354aeb1b7eb167d47f0612c29065502ae76ca
Arun Balajiee Lekshmi Narayanan <ARL122@pitt.edu>
2021-04-18 21:04:35 -0400

    attention

--- model3.py
+++ model3.py
@@ -50,8 +50,8 @@ class DecoderRNN(nn.Module):
     def forward(self, features, captions, lengths):
         """Decode image feature vectors and generates captions."""
         embeddings = self.embed(captions)
-        embeddings = torch.cat((features.unsqueeze(1), embeddings), 1)
-        packed = pack_padded_sequence(embeddings, lengths, batch_first=True) 
+        # embeddings = torch.cat((features.unsqueeze(1), embeddings), 1)
+        packed = pack_padded_sequence(embeddings, lengths.flatten(), batch_first=True) 
         hiddens, _ = self.lstm(packed)
         outputs = self.linear(hiddens[0])
         return outputs
