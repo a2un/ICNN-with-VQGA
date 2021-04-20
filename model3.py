e1ad428baa9701477cc828c3b0ea55626231cb2e
Arun Balajiee Lekshmi Narayanan <ARL122@pitt.edu>
2021-04-19 23:22:05 -0400

    test/validate

--- model3.py
+++ model3.py
@@ -119,7 +119,7 @@ class DecoderRNN(nn.Module):
         _, predicted = outputs.max(1)                        # predicted: (batch_size)
         inputs = self.embed(predicted)                       # inputs: (batch_size, embed_size)
         for i in range(self.max_seg_length):
-            # packed = pack_padded_sequence(inputs, lengths.flatten(), batch_first=True) 
+            packed = pack_padded_sequence(inputs, lengths.flatten(), batch_first=True) 
             hiddens, states = self.lstm(packed)          # hiddens: (batch_size, 1, hidden_size)
             # attention_weighted_encoding = attention(layer_features, hiddens[0])
             # attention_weighted_encoding = attention_weighted_encoding * self.sigmoid(hiddens[0])
